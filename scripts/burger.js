fa701de2c8f28320e93f924a456fd7feb2bc4d3f
jpegviolence <georgysemenovisthicc@gmail.com>
2023-05-19 17:58:14 +0300

    aaaaaaa

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,9 +1,18 @@
 const burgerBG = document.querySelector('.burger-bg')
 const burgerBtn = document.querySelector('.burger-btn')
 const headerShow = document.querySelector('.header_content')
+const body = document.body
 
 burgerBtn.addEventListener('click', function(){
     burgerBG.classList.toggle('active')
     headerShow.classList.toggle('active')
     this.classList.toggle('active')
+    body.classList.toggle('scroll-disable')
 })
+
+window.addEventListener("orientationchange", function () {
+    body.classList.remove('scroll-disable')
+    burgerBG.classList.remove('active')
+    burgerBtn.classList.remove('active')
+    headerShow.classList.remove('active')
+  })
