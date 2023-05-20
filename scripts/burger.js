11705c133683ab1fee0b1780a5439fe9f23fbbd5
jpegviolence <georgysemenovisthicc@gmail.com>
2023-05-21 02:03:43 +0300

    comments2

--- scripts/burger.js
+++ scripts/burger.js
@@ -3,6 +3,7 @@ const burgerBtn = document.querySelector('.burger-btn')
 const headerShow = document.querySelector('.header_content')
 const body = document.body
 
+// активирую бургер меню по клику
 burgerBtn.addEventListener('click', function(){
     burgerBG.classList.toggle('active')
     headerShow.classList.toggle('active')
@@ -10,6 +11,7 @@ burgerBtn.addEventListener('click', function(){
     body.classList.toggle('scroll-disable')
 })
 
+// убираю бургер меню при смене ориентации
 window.addEventListener("orientationchange", function () {
     body.classList.remove('scroll-disable')
     burgerBG.classList.remove('active')
--- scripts/popup.js
+++ scripts/popup.js
@@ -3,23 +3,28 @@ const popupImg = document.querySelector('div.popup-content > img')
 const popupField = document.querySelector('.popup-field')
 const closeBtn = document.querySelector('.popup-close-btn')
 
+// перебираю все ячейки галереи и ищу на какую конкретно нажали 
 allItems.forEach(item => item.addEventListener('click', function (e) {
-    clickedImg = item.querySelector('img').getAttribute('src')
-    popupImg.setAttribute('src', clickedImg)
+    clickedImg = item.querySelector('img').getAttribute('src') //получаю путь картинки, на которую кликнули
+    popupImg.setAttribute('src', clickedImg) //передаю его картинке в попапе
 
-    popupField.style.visibility = 'visible'
+    // делаю попап видимым
+    popupField.style.visibility = 'visible' 
     popupField.style.opacity = '1'
 }))
 
+// функция для закрытия попапа (делаю его невидимым)
 function closePopup(){
     popupField.style.visibility = 'hidden'
     popupField.style.opacity = '0'
 }
 
+// закрываем попап при нажатии на кнопку
 closeBtn.addEventListener('click', function () {
     closePopup()
 })
 
+// закрываем попап при нажатии на любой участок попапа
 popupField.addEventListener('click', function () {
     closePopup()
 })
